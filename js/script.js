d3233e0285ded4ea487be68bba4af70edb6244f2
hyomin <johyomin1119@gmail.com>
2022-07-17 02:21:06 +0900

    visual section 작업중

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
-window.onload = function {
+$(document).ready(function () {
+    $('html').css('overflow', 'hidden');
 
-}
+    let modalWrap = $('.modal-wrap');
+    let modalClose = $('.modal-close');
+    modalClose.click(function () {
+        modalWrap.stop().fadeOut(100);
+        $('html').css('overflow', 'auto');
+    });
+
+    let modalMain = $('.modal-main');
+    modalMain.click(function (event) {
+        event.stopPropagation();
+    });
+    modalWrap.click(function () {
+        modalWrap.stop().fadeOut(100);
+        $('html').css('overflow', 'auto');
+    });
+
+});
+
+window.onload = function () {
+    // swiper
+    new Swiper('.sw-visual', {
+        effect: "fade",
+        loop: true,
+        autoplay: {
+        delay: 10000,
+    },
+        navigation: {
+            prevEl: '.sw-visual-prev',
+            nextEl: '.sw-visual-next'
+        },
+
+        pagination: {
+            el: '.sw-visual-pg',
+            clickable: true
+        }
+    });
+
+};
