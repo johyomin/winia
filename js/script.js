f18c395297632f602f4723b54f902c187a12c171
hyomin <johyomin1119@gmail.com>
2022-07-26 01:30:45 +0900

    swiper 작업중

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
-$(document).ready(function () {
-    $('html').css('overflow', 'hidden');
+// $(document).ready(function () {
+//     $('html').css('overflow', 'hidden');
 
-    let modalWrap = $('.modal-wrap');
-    let modalClose = $('.modal-close');
-    modalClose.click(function () {
-        modalWrap.stop().fadeOut(100);
-        $('html').css('overflow', 'auto');
-    });
+//     let modalWrap = $('.modal-wrap');
+//     let modalClose = $('.modal-close');
+//     modalClose.click(function () {
+//         modalWrap.stop().fadeOut(100);
+//         $('html').css('overflow', 'auto');
+//     });
 
-    let modalMain = $('.modal-main');
-    modalMain.click(function (event) {
-        event.stopPropagation();
-    });
-    modalWrap.click(function () {
-        modalWrap.stop().fadeOut(100);
-        $('html').css('overflow', 'auto');
-    });
+//     let modalMain = $('.modal-main');
+//     modalMain.click(function (event) {
+//         event.stopPropagation();
+//     });
+//     modalWrap.click(function () {
+//         modalWrap.stop().fadeOut(100);
+//         $('html').css('overflow', 'auto');
+//     });
 
-});
+// });
 
 window.onload = function () {
     // swiper
-    new Swiper('.sw-visual', {
+    let swvisual = new Swiper('.sw-visual', {
         effect: "fade",
         loop: true,
         autoplay: {
-        delay: 10000,
-    },
+            delay: 10000,
+        },
         navigation: {
             prevEl: '.sw-visual-prev',
             nextEl: '.sw-visual-next'
@@ -38,4 +38,46 @@ window.onload = function () {
         }
     });
 
+    // 자동 실행 멈춤/재생
+    let swPause = $('.sw-visual-pause');
+    swPause.click(function () {
+
+        let temp = $(this).hasClass('sw-visual-play');
+        if (temp == false) {
+
+            $(this).addClass('sw-visual-play');
+            // 슬라이드 멈추기
+            swvisual.autoplay.stop();
+
+        } else {
+
+            $(this).removeClass('sw-visual-play');
+            // 슬라이드 재생
+            swvisual.autoplay.start();
+
+        }
+
+
+
+
+    });
+
+
 };
+
+window.onload = function () {
+
+    let swMenu = new Swiper('.sw-menu', {
+        slidesPerView: 4
+    });
+
+    let swItem = new Swiper('.sw-item', {
+        loop: true,
+        thumbs: {
+            swiper: swMenu,
+        },
+    });
+
+
+
+}
