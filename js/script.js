d29a64255f5b7b1c9b04a7863947583c56cd12fe
johyomin <johyomin1119@gmail.com>
2022-08-14 02:43:59 +0900

    social 수정중

--- js/script.js
+++ js/script.js
@@ -91,8 +91,17 @@ window.onload = function () {
 
 
     // popular sw
+    // 클래스 popular menu 의 a태그를 변수로 선택하자
     let popularMenu = $('.popular-menu a');
+    // popularMenu는 각각기능을한다
+    // index는 순서이다 컴퓨터는 0번부터 시작한다
+    // item은 html 의 태그이다
+    // popularMenu의 태그가 순서대로 기능을 각각한다
     $.each(popularMenu, function (index, item) {
+        // 이것은  popularMenu이다
+        // 클릭하면 발생한다
+        // a태그를 막아주고
+        // 슬라이드를 하나씩 이동한다.
         $(this).click(function (event) {
             // href 막기
             event.preventDefault();
@@ -114,26 +123,47 @@ window.onload = function () {
 
 
     // ap sw
+    // 클래스 ap-menu 의 모든 a태그를 변수 apMenu에 저장
     let apMenu = $('.ap-menu a');
+    // apMenu는 각각 기능을한다
+    // index는 0부터 시작하는 숫자
+    // item은 html의 태그를 뜻함
     $.each(apMenu, function (index, item) {
         $(this).click(function (event) {
             // href 막기
             event.preventDefault();
-            swApKit.slideTo(index + 1);
+            swAp.slideTo(index + 1);
         });
     });
 
 
     apMenu.eq(0).addClass('ap-menu-active');
 
-    let swApKit = new Swiper('.sw-ap', {});
 
-    swApKit.on('slideChange', function () {
-        apMenu.removeClass('ap-menu-active');
-        apMenu.eq(swApKit.realIndex).addClass('ap-menu-active');
-    });
+    // let swAp = new Swiper('.sw-ap', {});
+
+    // swAp.on('slideChange', function () {
+    //     apMenu.removeClass('ap-menu-active');
+    //     apMenu.eq(swAp.realIndex).addClass('ap-menu-active');
+    // });
+
+
+
+
 
     // social sw
     let swiper = new Swiper(".sw-social", {});
+
+    // 소셜 탭 기능
+    let recipeMenuA = $('.recipe-menu a');
+    let recipeCont = $('.recipe-cont');
+    $.each(recipeMenuA, function(index, item){
+        
+        $(this).mouseenter(function(){
+            recipeCont.stop().fadeOut();
+            recipeCont.eq(index).stop().fadeIn();
+        });
+
+    });
 
 }
